0734989ce0aca8b66476abf0431dee6ed17ea52c
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-03-02 15:37:56 +0900

    feat : firebase연동

---
+++ fair/src/fbase.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+// import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+// 파이어베이스 초기화
+// 참조 필요없으니 export 안함
+firebase.initializeApp(firebaseConfig);
+
+export const firebaseInstance = firebase;
+
+// // 로그인은 참조 할것이니 export 진행
+// export const authService = firebase.auth();
+
+// export const dbService = getFirestore();
